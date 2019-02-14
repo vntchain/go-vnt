9d0c6de5480c9b6ef76433b8331fe1c2359dec8d
zhangws <zhangws@yunphant.com>
2019-02-14 17:05:48 +0800

    fixed wrong modification

--- accounts/abi/key.go
+++ accounts/abi/key.go
@@ -53,7 +53,7 @@ type Root struct {
 type Node struct {
 	FieldName     string           `json:"name"`
 	FieldType     string           `json:"type"`
-	FieldLocation string           `json:"_"`
+	FieldLocation string           `json:"-"`
 	StorageType   StorageType      `json:"storagetype"`
 	Children      map[string]*Node `json:"children"`
 	Tables        []*Node          `json:"tables"`
