e4b20eb6687883c00dde4c037fe83d012a57f32f
zhangws <zhangws@yunphant.com>
2019-03-14 16:00:27 +0800

    remove log

--- internal/vntapi/api.go
+++ internal/vntapi/api.go
@@ -1287,7 +1287,6 @@ func (s *PublicTransactionPoolAPI) SendTransaction(ctx context.Context, args Sen
 // SendRawTransaction will add the signed transaction to the transaction pool.
 // The sender is responsible for signing the transaction and using the correct nonce.
 func (s *PublicTransactionPoolAPI) SendRawTransaction(ctx context.Context, encodedTx hexutil.Bytes) (common.Hash, error) {
-	log.Debug("api", "SendRawTransaction", encodedTx)
 	tx := new(types.Transaction)
 	if err := rlp.DecodeBytes(encodedTx, tx); err != nil {
 		return common.Hash{}, err
