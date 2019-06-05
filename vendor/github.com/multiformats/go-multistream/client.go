b484ee5d7bcb4e2debbd8bcffa64bb9dd0f1b7b0
Feng Xi Yan <goodyfx@163.com>
2019-06-05 14:20:24 +0800

    add debug

--- vendor/github.com/multiformats/go-multistream/client.go
+++ vendor/github.com/multiformats/go-multistream/client.go
@@ -26,9 +26,9 @@ func SelectProtoOrFail(proto string, rwc io.ReadWriteCloser) error {
 // SelectOneOf will perform handshakes with the protocols on the given slice
 // until it finds one which is supported by the muxer.
 func SelectOneOf(protos []string, rwc io.ReadWriteCloser) (string, error) {
-	fmt.Printf("#### client.go: SelectOneOf: protos: %v", protos)
+	fmt.Printf("#### client.go: SelectOneOf: protos: %v \n", protos)
 	err := handshake(rwc)
-	fmt.Printf("#### client.go: SelectOneOf finished: err: %v", err)
+	fmt.Printf("#### client.go: SelectOneOf finished: err: %v \n", err)
 	if err != nil {
 		return "", err
 	}
@@ -49,14 +49,14 @@ func SelectOneOf(protos []string, rwc io.ReadWriteCloser) (string, error) {
 func handshake(rwc io.ReadWriteCloser) error {
 	errCh := make(chan error, 1)
 	go func() {
-		fmt.Printf("#### client.go: handshake: will delimWriteBuffered")
+		fmt.Printf("#### client.go: handshake: will delimWriteBuffered \n")
 		errCh <- delimWriteBuffered(rwc, []byte(ProtocolID))
 	}()
 
 	tok, readErr := ReadNextToken(rwc)
-	fmt.Printf("#### client.go: handshake: ReadNextToken: token: %s, err: %v", tok, readErr)
+	fmt.Printf("#### client.go: handshake: ReadNextToken: token: %s, err: %v \n", tok, readErr)
 	writeErr := <-errCh
-	fmt.Printf("#### client.go: handshake: ReadNextToken: writeErr: %v", writeErr)
+	fmt.Printf("#### client.go: handshake: ReadNextToken: writeErr: %v \n", writeErr)
 
 	if writeErr != nil {
 		return writeErr
